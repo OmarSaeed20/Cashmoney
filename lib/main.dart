971bc0b502a57a0f1966cd98d04b3c0c68eaa614
OmarSaeed20 <omarhamode106@gmail.com>
2022-09-23 14:07:58 +0200

    ms

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:cashmoney/screens/git_card_screen.dart';
-import 'package:cashmoney/screens/login_screen.dart';
-import 'package:cashmoney/screens/sign2_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
